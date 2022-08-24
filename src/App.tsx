f2db3696bcef660c9e67a8c43b38b9ece38a617c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-08-24 21:41:26 +0530

    Create Cell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import NQueenService from "./service/n-queen-service";
+import './App.css';
+import {useRef} from "react";
 
 export default function App() {
+    const queens = useRef<HTMLSelectElement>();
+
     function handle() {
         const nQueenService = new NQueenService(8);
         const positions = nQueenService.getPositions()
         console.log(positions);
     }
 
-    return <>
-        <h1>N Queen Visualization</h1>
-        <button onClick={handle}>
-            Solve
-        </button>
-    </>
+    function onQueensChange() {
+        console.log(queens);
+    }
+
+    const element = <>
+        <div className="container">
+            <select
+                name="queens"
+                id="queens"
+                placeholder="Select number of queens"
+                ref={queens}
+                onChange={onQueensChange}
+            >
+                <option value="4">4 Queens</option>
+                <option value="8">8 Queens</option>
+            </select>
+        </div>
+    </>;
 }
---
+++ src/component/Cell.tsx
@@ -0,0 +1,7 @@
+import './Cell.css';
+
+export default function Cell() {
+    return (
+        <></>
+    );
+}
