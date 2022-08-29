81cdf35e2b4d3801f1033eb8cd63994cf98c9962
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-08-29 20:45:22 +0530

    Create queen section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {useRef} from "react";
 
 export default function App() {
-    const queens = useRef<HTMLSelectElement>();
+    const queens = useRef<HTMLSelectElement>(null);
 
     function handle() {
         const nQueenService = new NQueenService(8);
@@ -12,21 +12,35 @@ export default function App() {
     }
 
     function onQueensChange() {
-        console.log(queens);
     }
 
-    const element = <>
-        <div className="container">
-            <select
-                name="queens"
-                id="queens"
-                placeholder="Select number of queens"
-                ref={queens}
-                onChange={onQueensChange}
-            >
-                <option value="4">4 Queens</option>
-                <option value="8">8 Queens</option>
-            </select>
-        </div>
-    </>;
+    function getSelectedQueens(): number {
+        return parseInt(queens!.current!.value);
+    }
+
+    function start() {
+        if (!queens!.current!.value) {
+            queens!.current!.value = "4";
+        }
+
+        console.log(getSelectedQueens());
+    }
+
+    return (
+        <>
+            <div className="container">
+                <select
+                    name="queens"
+                    id="queens"
+                    placeholder="Select number of queens"
+                    ref={queens}
+                    onChange={onQueensChange}
+                >
+                    <option value="4">4 Queens</option>
+                    <option value="8">8 Queens</option>
+                </select>
+                <button onClick={start}>Start</button>
+            </div>
+        </>
+    );
 }
