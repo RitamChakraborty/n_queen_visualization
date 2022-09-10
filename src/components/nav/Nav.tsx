0080ba3a2c64862ce0029c0187326fa8abe9db24
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-09-10 12:42:28 +0530

    Add queen sticker as logo

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,10 +2,12 @@ import QueenSelector from "../queen-selector/QueenSelector";
 import SpeedSlider from "../speed-slider/SpeedSlider";
 import Buttons from "../buttons/Buttons";
 import "./Nav.css";
+import Queen from "../queen/Queen";
 
 export default function Nav() {
     return (
         <nav>
+            <Queen/>
             <QueenSelector/>
             <SpeedSlider/>
             <Buttons/>
---
+++ src/components/queen/Queen.tsx
@@ -0,0 +1,8 @@
+import "./Queen.css";
+import queen from "../../assets/queen.png";
+
+export default function Queen() {
+    return (
+        <img id="Queen" src={queen} alt="Queen"/>
+    )
+}
